6ea8bcb7d4c4cca6ab7d157bdb5e48dab9d46625
rossetl <rosset.lorenzo@gmail.com>
2024-10-08 16:05:29 +0200

    update 2

--- doc/conf.py
+++ doc/conf.py
@@ -34,4 +34,4 @@ source_suffix = {
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'press'
-html_static_path = ['_static']
+#html_static_path = ['_static']
